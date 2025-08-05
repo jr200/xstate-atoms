71dac760b09cc5d5affda87e1364e9fcf9e0ff7e
jr200 <jayshan+git@gmail.com>
2025-08-05 23:49:49 +0100

    use set atom to avoid re-rendering the load table component

--- examples/react-test/src/components/DuckDbUpdater.tsx
+++ examples/react-test/src/components/DuckDbUpdater.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useAtom } from 'jotai'
+import { useSetAtom } from 'jotai'
 import { duckdbMachineAtom } from '@jr200/xstate-atoms'
 import payloadContent from '/payload.b64ipc_zlib.txt?raw'
 import { callbackTextAtom } from './atoms'
 
 export const DuckDbUpdater = () => {
-  const [, duckdbSend] = useAtom(duckdbMachineAtom)
-  const [, setCallbackText] = useAtom(callbackTextAtom)
+  const duckdbSend = useSetAtom(duckdbMachineAtom)
+  const setCallbackText = useSetAtom(callbackTextAtom)
 
   const handleUpdate = () => {
     const tableSpecName = 'test_table'
