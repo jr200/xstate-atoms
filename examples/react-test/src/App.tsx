ac64992b15ae5aa68617d2b5e166d8fe43bb69d0
jr200 <jayshan+git@gmail.com>
2025-08-05 22:53:50 +0100

    (wip) use atoms everywhere

--- examples/react-test/src/App.tsx
+++ examples/react-test/src/App.tsx
@@ -7,21 +7,27 @@ import { TemporalUpdater } from './components/TemporalUpdater'
 import { TemporalGranularityExample } from './components/TemporalGranularityExample'
 import { DuckDbCatalog } from './components/DuckDbCatalog'
 import { DuckDbUpdater } from './components/DuckDbUpdater'
+import { DuckDbStatus } from './components/DuckDbStatus'
 
 function App() {
   return (
     <div className='App'>
       <div className='flex flex-row gap-4'>
         <JotaiProvider>
-          <DuckDbUpdater />
-          <TemporalUpdater />
-          <DuckDbCatalog />
-          <NatsExample />
-          <DuckDbExample />
           <div className='flex flex-col gap-4 h-full'>
+            <DuckDbCatalog />
+            <DuckDbUpdater />
+          </div>
+          <div className='flex flex-col gap-4 h-full'>
+            <DuckDbExample />
+            <DuckDbStatus />
+          </div>
+          <div className='flex flex-col gap-4 h-full'>
+            <TemporalUpdater />
             <TemporalExample />
             <TemporalGranularityExample granularity='minute' />
           </div>
+          <NatsExample />
         </JotaiProvider>
       </div>
     </div>
