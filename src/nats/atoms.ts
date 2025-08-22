ee0c0670001c87f71ccb6f06262b8815d310eda0
jr200 <jayshan+git@gmail.com>
2025-08-22 19:27:24 +0100

    bugfix: correctly export types of natsMachine

--- src/nats/atoms.ts
+++ src/nats/atoms.ts
@@ -1,14 +1,19 @@
 import { atomWithActor, atomWithActorSnapshot } from 'jotai-xstate'
-import { natsMachine } from '@jr200/xstate-nats'
-import { AnyActor } from 'xstate'
-import { atom } from 'jotai'
+import { natsMachine, type NatsContext, type NatsEvent } from '@jr200/xstate-nats'
+import { Actor, AnyActor, StateMachine } from 'xstate'
+import { atom, WritableAtom } from 'jotai'
 
 // this pattern/workaround for accessing child states is from:
 // https://github.com/jotaijs/jotai-xstate/issues/11
-export const natsActorAtom = atomWithActor(natsMachine)
+
+// this is a workaround to get at least some information out of
+// the 'too complex' natsMachine type
+type NatsMachine = StateMachine<NatsContext, NatsEvent, any, any, any, any, any, any, any, any, any, any, any, any>
+
+export const natsActorAtom = atomWithActor(natsMachine) as WritableAtom<Actor<NatsMachine>, any, any>
 natsActorAtom.debugLabel = 'xa.natsActorAtom'
 
-export const natsSnapshotAtom = atomWithActorSnapshot(get => {
+export const natsSnapshotAtom = atomWithActorSnapshot<Actor<NatsMachine>>(get => {
   const snapshot = get(natsActorAtom)
   return snapshot
 })
